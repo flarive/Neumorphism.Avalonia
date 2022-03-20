a03d125a1e895d692913d85270dc6cbd8d3e4436
Eviral <flarive@hotmail.com>
2022-03-20 11:06:48 +0100

    Make it compile

--- Neumorphism.Demo/App.axaml.cs
+++ Neumorphism.Demo/App.axaml.cs
@@ -1,9 +1,12 @@
 ﻿using Avalonia;
 using Avalonia.Markup.Xaml;
 
-namespace Neumorphism.Demo {
-    public class App : Application {
-        public override void Initialize() {
+namespace Neumorphism.Demo
+{
+    public class App : Application
+    {
+        public override void Initialize()
+        {
             AvaloniaXamlLoader.Load(this);
         }
     }
--- Neumorphism.Styles/Converters/ControlWidthToBoxShadowConverter.cs
+++ Neumorphism.Styles/Converters/ControlWidthToBoxShadowConverter.cs
@@ -12,17 +12,18 @@ namespace Neumorphism.Styles.Converters
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             var b = new BoxShadows();
+            bool inset = parameter != null && parameter == "1";
 
             if (value is double)
             {
-                double width = (double)value;
-                if (width > 0)
+                double height = (double)value;
+                if (height > 0)
                 {
                     // for a 300x300 button shadow radius must be 60 (300/5);
-                    double radius = (double)(width / 5);
+                    double radius = (double)(height / 5);
 
                     // for a 300x300 button shadow offset must be 20 (300/15);
-                    double offset = (double)(width / 15);
+                    double offset = (double)(height / 15);
 
                     //-20 -20 60 #CCFFFFFF,20 20 60 #33000000
                     BoxShadow main = new BoxShadow();
@@ -37,9 +38,29 @@ namespace Neumorphism.Styles.Converters
                     rest1.Blur = radius;
                     rest1.Color = Color.Parse("#33000000");
 
+                    
+                    if (inset)
+                    {
+                        main.IsInset = true;
+                        rest1.IsInset = true;
+
+                        main.OffsetX = offset / 2;
+                        main.OffsetY = offset / 2;
+
+                        rest1.OffsetX = -offset / 2;
+                        rest1.OffsetY = -offset / 2;
+
+
+                        // invert shadow colors
+                        rest1.Color = Color.Parse("#CCFFFFFF");
+                        main.Color = Color.Parse("#33000000"); 
+                    }
+
+
                     List<BoxShadow> rest = new List<BoxShadow>() { rest1 };
 
                     b = new BoxShadows(main, rest.ToArray());
+                    
                 }
             }
 
