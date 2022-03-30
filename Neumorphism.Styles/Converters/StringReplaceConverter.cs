d2dcae4ab82c10798f3da5305f5f3654b918dbb7
flarive34rs <flarive@radioshop.net>
2022-03-30 13:51:33 +0200

    Fix toggle switch

--- Neumorphism.Styles/Converters/StringReplaceConverter.cs
+++ Neumorphism.Styles/Converters/StringReplaceConverter.cs
@@ -3,11 +3,10 @@ using System.Collections.Generic;
 using System.Globalization;
 using Avalonia;
 using Avalonia.Data.Converters;
-using Avalonia.Media;
 
 namespace Neumorphism.Styles.Converters
 {
-    public class WidthToRenderTransformConverter : IMultiValueConverter
+    public class StringReplaceConverter : IMultiValueConverter
     {
         public object Convert(IList<object> values, Type targetType, object parameter, CultureInfo culture)
         {
@@ -15,22 +14,20 @@ namespace Neumorphism.Styles.Converters
                 && values[0] != AvaloniaProperty.UnsetValue
                 && values[1] != AvaloniaProperty.UnsetValue)
             {
-                double width = (double)values[0];
-                double heigth = (double)values[1];
+                string primaryString = (string)values[0];
+                string secondaryString = (string)values[1];
 
-
-                var conv = new TransformConverter();
-
-                if (width is double.NaN || heigth is double.NaN)
+                if (!string.IsNullOrEmpty(primaryString))
                 {
-                    // no translate
-                    return conv.ConvertFromString("translate(0px, 0px)");
+                    return primaryString;
                 }
-
-                return conv.ConvertFromString("translate(" + (width - heigth) + "px, 0px)");
+                else if (!string.IsNullOrEmpty(secondaryString))
+                {
+                    return secondaryString;
+                }
             }
 
-            return new TranslateTransform(0, 0);
+            return string.Empty;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
