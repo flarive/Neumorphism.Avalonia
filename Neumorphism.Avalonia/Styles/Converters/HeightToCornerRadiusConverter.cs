1a53e4a8c3435acce3572921de459c634a264d78
flarive34rs <flarive@radioshop.net>
2022-06-10 17:13:46 +0200

    Sliders theming in progress

--- Neumorphism.Avalonia/Styles/Converters/HeightToCornerRadiusConverter.cs
+++ Neumorphism.Avalonia/Styles/Converters/HeightToCornerRadiusConverter.cs
@@ -9,11 +9,26 @@ namespace Neumorphism.Avalonia.Styles.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
+            bool isAll = parameter == null || parameter.Equals("0");
+            bool isLeftTopBottom = parameter == null || parameter.Equals("1");
+            bool isRightTopBottom = parameter == null || parameter.Equals("2");
+
             if (value is double)
             {
-                double height = (double)value;
+                double height = ((double)value) / 2;
 
-                return new CornerRadius(height / 2);
+                if (isAll)
+                {
+                    return new CornerRadius(height);
+                }
+                else if (isLeftTopBottom)
+                {
+                    return new CornerRadius(0, height, height, 0);
+                }
+                else if (isRightTopBottom)
+                {
+                    return new CornerRadius(height, 0, 0, height);
+                }
             }
 
             return new CornerRadius(0);
