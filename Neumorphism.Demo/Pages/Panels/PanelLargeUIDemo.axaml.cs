80aef72d87457c630c2c46df756f1758c5c84f63
flarive34rs <flarive@radioshop.net>
2022-07-01 17:56:37 +0200

    Combobox improvements (add left or right header) Combobox fixes when custom width/height used Sample UI panels wip (hidden for the moment)

---
+++ Neumorphism.Demo/Pages/Panels/PanelLargeUIDemo.axaml.cs
@@ -0,0 +1,21 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Neumorphism.Avalonia.Demo.ViewModels;
+
+namespace Neumorphism.Avalonia.Demo.Pages.Panels
+{
+    public class PanelLargeUIDemo : UserControl
+    {
+        public PanelLargeUIDemo()
+        {
+            this.InitializeComponent();
+
+            DataContext = new ButtonFieldsViewModel();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
---
+++ Neumorphism.Demo/Pages/Panels/PanelMediumUIDemo.axaml.cs
@@ -0,0 +1,21 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Neumorphism.Avalonia.Demo.ViewModels;
+
+namespace Neumorphism.Avalonia.Demo.Pages.Panels
+{
+    public class PanelMediumUIDemo : UserControl
+    {
+        public PanelMediumUIDemo()
+        {
+            this.InitializeComponent();
+
+            DataContext = new ButtonFieldsViewModel();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
