31512dde0e80fa98802b59d495c61175b8231fe2
flarive34rs <flarive@radioshop.net>
2022-07-13 17:21:10 +0200

    Add user card demo (work in progress)

---
+++ Neumorphism.Demo/Pages/Panels/PanelUserProfilDemo.axaml.cs
@@ -0,0 +1,21 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Neumorphism.Avalonia.Demo.ViewModels.Panels;
+
+namespace Neumorphism.Avalonia.Demo.Pages.Panels
+{
+    public class PanelUserProfilDemo : UserControl
+    {
+        public PanelUserProfilDemo()
+        {
+            this.InitializeComponent();
+
+            
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
