ab80df2e74c710cc58ba68b5cb585e7fc95c7f5d
Frédéric Larivé <frederic.larive@ubisoft.com>
2023-09-15 17:02:56 +0200

    TimePicker WIP

--- Avalonia.Themes.Neumorphism/Assists/TimePickerAssist.cs
+++ Avalonia.Themes.Neumorphism/Assists/TimePickerAssist.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Media;
 
 namespace Avalonia.Themes.Neumorphism.Assist
 {
@@ -9,5 +10,21 @@ namespace Avalonia.Themes.Neumorphism.Assist
         public static void SetUseFloatingWatermark(AvaloniaObject element, bool value) => element.SetValue(UseFloatingWatermarkProperty, value);
 
         public static bool GetUseFloatingWatermark(AvaloniaObject element) => (bool)element.GetValue(UseFloatingWatermarkProperty);
+
+
+        public static AvaloniaProperty<IBrush> InnerRightBackgroundProperty = AvaloniaProperty.RegisterAttached<CalendarDatePicker, IBrush>(
+            "InnerRightBackground", typeof(TimePickerAssist));
+
+        public static void SetInnerRightBackground(AvaloniaObject element, IBrush value) => element.SetValue(InnerRightBackgroundProperty, value);
+
+        public static IBrush GetInnerRightBackground(AvaloniaObject element) => (IBrush)element.GetValue(InnerRightBackgroundProperty);
+
+
+        public static AvaloniaProperty<Thickness> InnerRightPaddingProperty = AvaloniaProperty.RegisterAttached<CalendarDatePicker, Thickness>(
+            "InnerRightPadding", typeof(TimePickerAssist));
+
+        public static void SetInnerRightPadding(AvaloniaObject element, Thickness value) => element.SetValue(InnerRightPaddingProperty, value);
+
+        public static Thickness GetInnerRightPadding(AvaloniaObject element) => (Thickness)element.GetValue(InnerRightPaddingProperty);
     }
 }
