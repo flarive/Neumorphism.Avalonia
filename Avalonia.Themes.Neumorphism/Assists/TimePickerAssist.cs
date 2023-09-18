2ef02d0f309c1bc76960a38eaabef9c01c272423
Eviral <flarive@hotmail.com>
2023-09-18 22:12:39 +0200

    Date picker fixes

--- Avalonia.Themes.Neumorphism/Assists/TimePickerAssist.cs
+++ Avalonia.Themes.Neumorphism/Assists/TimePickerAssist.cs
@@ -1,4 +1,3 @@
-using Avalonia.Controls;
 using Avalonia.Media;
 using Avalonia.Themes.Neumorphism.Controls;
 
@@ -6,13 +5,6 @@ namespace Avalonia.Themes.Neumorphism.Assist
 {
     public static class TimePickerAssist
     {
-        //public static AvaloniaProperty<bool> UseFloatingWatermarkProperty = AvaloniaProperty.RegisterAttached<ExtendedTimePicker, bool>("UseFloatingWatermark", typeof(TimePickerAssist));
-
-        //public static void SetUseFloatingWatermark(AvaloniaObject element, bool value) => element.SetValue(UseFloatingWatermarkProperty, value);
-
-        //public static bool GetUseFloatingWatermark(AvaloniaObject element) => (bool)element.GetValue(UseFloatingWatermarkProperty);
-
-
         public static AvaloniaProperty<IBrush> InnerRightBackgroundProperty = AvaloniaProperty.RegisterAttached<ExtendedTimePicker, IBrush>("InnerRightBackground", typeof(TimePickerAssist));
 
         public static void SetInnerRightBackground(AvaloniaObject element, IBrush value) => element.SetValue(InnerRightBackgroundProperty, value);
