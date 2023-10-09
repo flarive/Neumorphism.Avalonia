dd37999c722f6747801ecfa5ce1827f5a1ba2c4c
Eviral <flarive@hotmail.com>
2023-10-09 21:05:33 +0200

    DateTimePicker bug fix in progress

---
+++ Avalonia.Themes.Neumorphism/Controls/ExtendedCalendarDatePicker.cs
@@ -0,0 +1,41 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using Avalonia.Reactive;
+using Avalonia.Controls.Metadata;
+using Avalonia.Controls.Primitives;
+using Avalonia.Data;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Controls;
+using Avalonia.Layout;
+using System.Globalization;
+using System.Reactive.Disposables;
+
+namespace Avalonia.Themes.Neumorphism.Controls
+{
+    public partial class ExtendedCalendarDatePicker : CalendarDatePicker
+    {
+        private TextBox? _textBox;
+
+
+        /// <inheritdoc/>
+        protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+        {
+            
+            _textBox = e.NameScope.Find<TextBox>("PART_TextBox");
+            _textBox.LostFocus += _textBox_LostFocus;
+        }
+
+        private void _textBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (_textBox.Text == "mmm")
+            {
+                _textBox.Text = "";
+            }
+        }
+
+    }
+}
