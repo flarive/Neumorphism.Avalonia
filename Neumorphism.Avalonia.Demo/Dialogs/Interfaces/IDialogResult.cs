cec73f6193ca8e1616b4d3d67f953cec1f7e390d
Eviral <flarive@hotmail.com>
2023-11-02 22:23:01 +0100

    Menus are now fully themed !

--- Neumorphism.Avalonia.Demo/Dialogs/Interfaces/IDialogResult.cs
+++
@@ -1,7 +0,0 @@
-﻿//namespace Neumorphism.Avalonia.Demo.Dialogs.Interfaces
-//{
-//    public interface IDialogResult
-//    {
-//        string GetResult { get; }
-//    }
-//}
--- Neumorphism.Avalonia.Demo/Dialogs/Interfaces/IHasNegativeResult.cs
+++
@@ -1,7 +0,0 @@
-﻿//namespace Neumorphism.Avalonia.Demo.Dialogs.Interfaces
-//{
-//    public interface IHasNegativeResult
-//    {
-//        void SetNegativeResult(DialogResult result);
-//    }
-//}
