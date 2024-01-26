04de548b0bb60a2aa8c18ad932666ae7381ba00a
Frédéric Larivé <frederic.larive@ubisoft.com>
2024-01-26 17:26:39 +0100

    Start theming Expanders

---
+++ Neumorphism.Avalonia.Demo/Pages/ExpanderDemo.axaml.cs
@@ -0,0 +1,15 @@
+﻿using Avalonia.Controls;
+using Neumorphism.Avalonia.Demo.ViewModels;
+
+namespace Neumorphism.Avalonia.Demo.Pages
+{
+    public partial class ExpanderDemo : UserControl
+    {
+        public ExpanderDemo()
+        {
+            InitializeComponent();
+
+            DataContext = new ExpanderDemoViewModel();
+        }
+    }
+}
